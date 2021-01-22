af4fd93e34caf2e000cedf4a11f3f3fd4aa53767
hzuna <hzuna@localhost.localdomain>
2021-01-22 23:19:50 +0800

    deploy to heroku

---
+++ README.md
@@ -0,0 +1,6 @@
+
+#### First: Install heroku CLI [doc here] (https://devcenter.heroku.com/articles/heroku-cli)
+
+#### (optional) Build your docker image through top directory.
+#### Next chdir to deploy, run command `heroku login -i`,
+#### then run command `sh autoinstall.sh`, wait few minutes, all be done!
---
+++ deploy/autoinstall.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo This is a automatic script that deploys service to heroku based on alpine,novnc
+
+heroku create
+
+echo Type your application name:
+read appname
+
+echo If your name is correct, you can get it via https://$appname.heroku.com when it completes.
+
+heroku container:login
+
+heroku container:push web --app $appname
+
+heroku container:release web --app $appname
+
+echo Everything's done, https://$appname.herokuapp.com GO! , passwd presets by default is archlinux.
